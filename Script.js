c2c10a7987a34a5896cb3d8a1fac81c6d64e7be6
Roro <100329632+rodaH123@users.noreply.github.com>
2022-03-27 00:12:39 -0500

    Updated Script name in VS code and deleted Developer folder

--- Script.js
+++ Script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 //Arrays
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -62,12 +63,10 @@ function generatePassword(){
     possible = possible.concat(lowerCase)
     certain.push(getRandom(lowerCase))
   }
-​
   if (passOptions.numberConfirm) {
     possible = possible.concat(number)
     certain.push(getRandom(number))
   }
-​
   if (passOptions.symbolConfirm){
     possible = possible.concat(symbols)
     certain.push(getRandom(symbols))
@@ -76,11 +75,9 @@ function generatePassword(){
     var possibleChar = getRandom(possible)
     result.push(possibleChar)
   }
-​
   for(var i = 0; i < certain.length; i++){
     result[i] = certain[i]
   }
-​
   return result.join('')
 }
 function getRandom(arr){
@@ -91,4 +88,10 @@ function getRandom(arr){
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+ 
+ 
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
